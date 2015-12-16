9b6b1d538ec09cc219ef2ad38a82221be2b002e2
lanbau <irvinln@gmail.com>
2015-12-16 16:41:50 +0800

    ES6

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,37 +1,38 @@
+import React, { Component } from 'react'
 // SearchBox Component, does not run until you call it
-export default SearchBox = React.createClass({
+export default class SearchBox extends Component {
 
-  // this runs first
-  getInitialState:function(){
-    return {
+  constructor (props) {
+    super(props)
+    this.state = {
       searchingFor: ''
-    };
-  },
+    }
+  }
 
-  render: function() {
+  render () {
     return (
       <div className='searchbox' >
         // input box
-        <input onChange={this.whenUserTypes} type='text'
+        <input onChange={::this.whenUserTypes} type='text'
         placeholder='Just Search Lah...'/>
 
         // This will appear upon typing "Searching For ..."
         {this.getSearchingForDom()}
       </div>
-    );
+    )
 
-  },
+  }
 
-  getSearchingForDom: function() {
+  getSearchingForDom() {
     // set this.state in the next function
     const {searchingFor} = this.state;
     if (searchingFor) {
         return <div>Searching for ... {searchingFor}</div>;
     }
     return null;
-  },
+  }
 
-  whenUserTypes: function (e) {
+  whenUserTypes(e) {
     const query = e.target.value;
 
     // set the state
@@ -39,4 +40,4 @@ export default SearchBox = React.createClass({
       searchingFor: query
     });
   }
-});
+};
--- dist/app.js
+++ dist/app.js
@@ -19652,60 +19652,88 @@
 
 /***/ },
 /* 159 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	// SearchBox Component, does not run until you call it
 	'use strict';
 
 	Object.defineProperty(exports, '__esModule', {
 	  value: true
 	});
-	exports['default'] = SearchBox = React.createClass({
-	  displayName: 'SearchBox',
 
-	  // this runs first
-	  getInitialState: function getInitialState() {
-	    return {
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	// SearchBox Component, does not run until you call it
+
+	var SearchBox = (function (_Component) {
+	  _inherits(SearchBox, _Component);
+
+	  function SearchBox(props) {
+	    _classCallCheck(this, SearchBox);
+
+	    _get(Object.getPrototypeOf(SearchBox.prototype), 'constructor', this).call(this, props);
+	    this.state = {
 	      searchingFor: ''
 	    };
-	  },
+	  }
 
-	  render: function render() {
-	    return React.createElement(
-	      'div',
-	      { className: 'searchbox' },
-	      '// input box',
-	      React.createElement('input', { onChange: this.whenUserTypes, type: 'text',
-	        placeholder: 'Just Search Lah...' }),
-	      '// This will appear upon typing "Searching For ..."',
-	      this.getSearchingForDom()
-	    );
-	  },
-
-	  getSearchingForDom: function getSearchingForDom() {
-	    // set this.state in the next function
-	    var searchingFor = this.state.searchingFor;
-
-	    if (searchingFor) {
-	      return React.createElement(
+	  _createClass(SearchBox, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2['default'].createElement(
 	        'div',
-	        null,
-	        'Searching for ... ',
-	        searchingFor
+	        { className: 'searchbox' },
+	        '// input box',
+	        _react2['default'].createElement('input', { onChange: this.whenUserTypes.bind(this), type: 'text',
+	          placeholder: 'Just Search Lah...' }),
+	        '// This will appear upon typing "Searching For ..."',
+	        this.getSearchingForDom()
 	      );
 	    }
-	    return null;
-	  },
+	  }, {
+	    key: 'getSearchingForDom',
+	    value: function getSearchingForDom() {
+	      // set this.state in the next function
+	      var searchingFor = this.state.searchingFor;
 
-	  whenUserTypes: function whenUserTypes(e) {
-	    var query = e.target.value;
+	      if (searchingFor) {
+	        return _react2['default'].createElement(
+	          'div',
+	          null,
+	          'Searching for ... ',
+	          searchingFor
+	        );
+	      }
+	      return null;
+	    }
+	  }, {
+	    key: 'whenUserTypes',
+	    value: function whenUserTypes(e) {
+	      var query = e.target.value;
 
-	    // set the state
-	    this.setState({
-	      searchingFor: query
-	    });
-	  }
-	});
+	      // set the state
+	      this.setState({
+	        searchingFor: query
+	      });
+	    }
+	  }]);
+
+	  return SearchBox;
+	})(_react.Component);
+
+	exports['default'] = SearchBox;
+	;
 	module.exports = exports['default'];
 
 /***/ }
